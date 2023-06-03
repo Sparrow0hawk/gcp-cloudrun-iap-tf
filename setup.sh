c896e582e157b19e1ead66934f9a105b6285b73a
Sparrow0hawk <alexjcoleman@gmail.com>
2023-06-03 12:58:02 +0100

    gcloud setup in script form

---
+++ setup.sh
@@ -0,0 +1,26 @@
+
+# create project in gcp
+gcloud projects create test-cloud-run-iap --name="Test terra shiny"
+
+# set current project with gcloud as this project
+gcloud config set project test-cloud-run-iap
+
+gcloud beta billing projects link test-cloud-run-iap \
+ --billing-account $BILLING_ACCOUNT_ID
+
+gcloud services enable \
+    run.googleapis.com \
+    iap.googleapis.com
+    #containerregistry.googleapis.com \
+    #compute.googleapis.com \
+    #vpcaccess.googleapis.com
+
+gcloud iam service-accounts create terrashiny \
+ --display-name "Terra shiny service account"
+
+gcloud projects add-iam-policy-binding test-cloud-run-iap \
+    --member="serviceAccount:terrashiny@test-cloud-run-iap.iam.gserviceaccount.com" \
+    --role="roles/editor"
+
+gcloud iam service-accounts keys create .credentials.json \
+    --iam-account=terrashiny@test-cloud-run-iap.iam.gserviceaccount.com
