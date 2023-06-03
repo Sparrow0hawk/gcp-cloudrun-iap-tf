4555dccddf666d334fa79b4d587089f77019945a
Sparrow0hawk <alexjcoleman@gmail.com>
2023-06-03 13:51:30 +0100

    add IAM policy to make app public
    this change requires the service account has the owner role

--- main.tf
+++ main.tf
@@ -29,3 +29,19 @@ resource "google_cloud_run_v2_service" "default" {
     }
   }
 }
+
+# add a iam policy to make app public
+data "google_iam_policy" "noauth" {
+  binding {
+    role    = "roles/run.invoker"
+    members = ["allUsers"]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_v2_service.default.location
+  project  = google_cloud_run_v2_service.default.project
+  service  = google_cloud_run_v2_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
