06b4534d5ad08f3652f7b5d144658e31ff646278
Sparrow0hawk <alexjcoleman@gmail.com>
2023-06-03 13:34:41 +0100

    correct location field in cloud run service

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "google" {
 
 resource "google_cloud_run_v2_service" "default" {
   name     = "cloudrun-service"
-  location = var.zone
+  location = var.region
   ingress  = "INGRESS_TRAFFIC_ALL"
 
   template {
