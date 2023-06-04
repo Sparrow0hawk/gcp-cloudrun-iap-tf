21c834eb7e233beea440d13581ea742edf7e55ab
Sparrow0hawk <alexjcoleman@gmail.com>
2023-06-04 13:52:36 +0100

    change to use artifact registry

--- push-container/main.tf
+++ push-container/main.tf
@@ -15,9 +15,11 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_container_registry" "registry" {
-  project  = var.project
-  location = var.region
+resource "google_artifact_registry_repository" "my-repo" {
+  location      = var.region
+  repository_id = join("", [var.project, "-ecr"])
+  description   = "example docker repository"
+  format        = "DOCKER"
 
   provisioner "local-exec" {
     command = "ansible-playbook main-playbook.yml -e 'credential_file=${var.credentials_file} registry_url=${google_container_registry.registry.bucket_self_link} image_name=${var.image_name}'"
